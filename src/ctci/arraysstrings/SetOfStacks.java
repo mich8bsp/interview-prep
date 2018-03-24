8dba851b4b4f4a1a9b7e7c1cdb384744b1228403
mich8bsp <mich8bsp@gmail.com>
2018-03-25 00:22:14 +0300

    implement set of stacks

---
+++ src/ctci/arraysstrings/SetOfStacks.java
@@ -0,0 +1,55 @@
+package ctci.arraysstrings;
+
+import java.util.Stack;
+
+public class SetOfStacks<T> {
+
+    private static final int MAX_STACK_SIZE = 16;
+
+    private Stack<Stack<T>> stacks = new Stack<>();
+
+    public T push(T item) {
+        Stack<T> latestStack = stacks.peek();
+        if (latestStack == null || latestStack.size() == MAX_STACK_SIZE) {
+            latestStack = new Stack<>();
+            stacks.push(latestStack);
+        }
+        latestStack.push(item);
+        return item;
+    }
+
+    public synchronized T pop() {
+        Stack<T> latestStack = findLastNonEmpty();
+        if (latestStack == null) {
+            return null;
+        }
+        T popped = latestStack.pop();
+        if(latestStack.isEmpty()){
+            stacks.pop();
+        }
+        return popped;
+    }
+
+    private Stack<T> findLastNonEmpty(){
+        Stack<T> latestStack = stacks.peek();
+        while (latestStack != null && latestStack.isEmpty()) {
+            stacks.pop();
+            latestStack = stacks.peek();
+        }
+        return latestStack;
+    }
+
+    public boolean empty() {
+        return stacks.isEmpty();
+    }
+
+
+    public synchronized T peek() {
+        Stack<T> latestStack = findLastNonEmpty();
+        if (latestStack == null) {
+            return null;
+        }
+        return latestStack.peek();
+    }
+
+}
